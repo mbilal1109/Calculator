e78b26c1c7be03d17e9779f3273e003f09fd716f
Mohammad Bilal <42048110+mbilal1109@users.noreply.github.com>
2023-12-14 16:29:37 -0600

    Adding initial files

---
+++ calculator.js
@@ -0,0 +1,32 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operation = '';
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operator(firstNumber, secondNumber, operation) {
+    switch(operation) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+    }
+}
