90b5a24aec3b0a832c190b6d2f680d4786a4e8ef
Mohammad Bilal <42048110+mbilal1109@users.noreply.github.com>
2023-12-22 14:17:55 -0600

    Ability to Calculate Two Pairs of Numbers

--- calculator.js
+++ calculator.js
@@ -1,10 +1,16 @@
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber = "0";
+let secondNumber = "";
 let operation = "";
-let userInput = 0;
+let userInput = "";
+let inputArray = [];
+let result = 0;
 
+const calculatorButtons = document.querySelectorAll(".calculator-button");
 const numbers = document.querySelectorAll(".number");
 const display = document.querySelector("#screen-number");
+const equalButton = document.querySelector(".equal");
+const symbols  = document.querySelectorAll(".symbol");
+const clearBtn = document.querySelector(".clear");
 
 function add(num1, num2) {
     return num1 + num2;
@@ -35,8 +41,37 @@ function operator(firstNumber, secondNumber, operation) {
     }
 }
 
-numbers.forEach((number) => {
-    number.addEventListener("click", function() {
-        display.textContent = number.value;
+function populateDisplay(toBeDisplayed) {
+    display.textContent = toBeDisplayed;
+}
+
+function getResult() {
+  equalButton.addEventListener("click", function () {
+    result = operator(parseInt(inputArray[0]), parseInt(inputArray[2]), inputArray[1]);
+  });
+}
+
+calculatorButtons.forEach((button) => {
+    button.addEventListener("click", function() {
+        if (
+          button.value == "+" ||
+          button.value == "-" ||
+          button.value == "*" ||
+          button.value == "/"
+        ) {
+          inputArray.push(userInput);
+          inputArray.push(button.value);
+          userInput = "";
+        } else if (button.value == "=") {
+          inputArray.push(userInput);
+          populateDisplay(operator(parseInt(inputArray[0]), parseInt(inputArray[2]), inputArray[1]));
+        } else {
+          userInput = userInput + button.value;
+          populateDisplay(userInput);
+        }
     });
+});
+
+clearBtn.addEventListener("click", function() {
+    window.location.reload();
 });
